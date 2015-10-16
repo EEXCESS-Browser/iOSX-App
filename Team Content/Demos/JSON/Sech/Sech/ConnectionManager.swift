0744909995a8671fefd621aa6e72be9f8f95ab7f
apoehlmann <alexander.poehlmann@hof-university.de>
2015-10-16 13:21:20 +0200

    add ConnectionManager
    alter Code ist aus kommentiert.

---
+++ Team Content/Demos/JSON/Sech/Sech/ConnectionManager.swift
@@ -0,0 +1,58 @@
+//
+//  ConnectionManager.swift
+//  Sech
+//
+//  Created by Alexander Pöhlmann on 16.10.15.
+//  Copyright © 2015 Peter Stoehr. All rights reserved.
+//
+
+import Foundation
+
+class ConnectionManager{
+    
+    static let POST = "POST"
+    static let GET = "GET"
+    
+    let JSON_OPTION = "application/json"
+    
+    let FOR_HTTP_HEADER_FIELD__CONTENT_TYPE = "Content-Type"
+    let FOR_HTTP_HEADER_FIELD__ACCENT = "Accept"
+    
+    func makeHTTP_Request(data:JSON,url:String,httpMethod:String,postCompleted : (succeeded: Bool, data: NSData) -> ()){
+        let request = addRequestContent(createRequest(url),httpMethod: httpMethod,data: data.convertToNSData()!)
+        self.sendRequest(request, postCompleted: postCompleted)
+    }
+    
+    private func createRequest(url:String)->NSMutableURLRequest{
+        return NSMutableURLRequest(URL: NSURL(string: url)!)
+    }
+    
+    private func addRequestContent(request:NSMutableURLRequest,httpMethod:String,data:NSData)->NSMutableURLRequest{
+        request.HTTPMethod = httpMethod
+        request.addValue(JSON_OPTION, forHTTPHeaderField: FOR_HTTP_HEADER_FIELD__CONTENT_TYPE)
+        request.addValue(JSON_OPTION, forHTTPHeaderField: FOR_HTTP_HEADER_FIELD__ACCENT)
+        request.HTTPBody = data
+        return request
+    }
+    
+    private func sendRequest(request:NSMutableURLRequest,postCompleted : (succeeded: Bool, data: NSData) -> ()){
+        let session = NSURLSession.sharedSession()
+        let task = session.dataTaskWithRequest(request, completionHandler: {data, response, error -> Void in
+            postCompleted(succeeded: error == nil, data: data!)
+        })
+        
+        
+        task.resume()
+    }
+}
+
+
+
+class URLs {
+    static let url1 = ""
+    static let url2 = ""
+    static let url3 = ""
+    static let url4 = ""
+}
+
+
--- Team Content/Demos/JSON/Sech/Sech/ViewController.swift
+++ Team Content/Demos/JSON/Sech/Sech/ViewController.swift
@@ -16,7 +16,8 @@ class ViewController: NSViewController, NSTextFieldDelegate {
     @IBOutlet var response: NSTextView!
 
 
-    var connection : Connection!
+//    var connection : Connection!
+    var connectionManager:ConnectionManager!
     var msg:NSData? = nil
     
     @IBOutlet weak var searchTextField: NSTextField!
@@ -35,33 +36,61 @@ class ViewController: NSViewController, NSTextFieldDelegate {
 
     private func doStuff()
     {
-        let jsonObject : [String:AnyObject] = [
-            "origin":["clientType" : "Swift-Test-Client",
-                "clientVersion" : "0.21",
-                "module" : "OS X Prototype",
-                "userID" : "PDPS-WS2015"],
-            "numResults":4,
-            "contextKeywords": [["text" : "\(recommendation.stringValue)"]]
-        ]
+//        let jsonObject : [String:AnyObject] = [
+//            "origin":["clientType" : "Swift-Test-Client",
+//                "clientVersion" : "0.21",
+//                "module" : "OS X Prototype",
+//                "userID" : "PDPS-WS2015"],
+//            "numResults":4,
+//            "contextKeywords": [["text" : "\(recommendation.stringValue)"]]
+//        ]
+//        connection.post(json.jsonObject, url: "https://eexcess-dev.joanneum.at/eexcess-privacy-proxy-1.0-SNAPSHOT/api/v1/recommend"){ (succeeded: Bool, msg: NSData) -> () in
+//            if(succeeded) {
+//                dispatch_async(dispatch_get_main_queue(), {
+//                    self.response.string = String(data: msg, encoding: NSUTF8StringEncoding)!
+//                    self.msg = msg
+//                })
+//            }
+//            else {
+//                self.response.string = "Error"
+//            }
+//        }
         
-        connection.post(jsonObject, url: "https://eexcess-dev.joanneum.at/eexcess-privacy-proxy-1.0-SNAPSHOT/api/v1/recommend"){ (succeeded: Bool, msg: NSData) -> () in
-            if(succeeded) {
-                dispatch_async(dispatch_get_main_queue(), {
-                    self.response.string = String(data: msg, encoding: NSUTF8StringEncoding)!
-                    self.msg = msg
-                })
-            }
-            else {
-                self.response.string = "Error"
-            }
-        }
-
+        let json = JSON()
+        let childJSON = JSON();
+        
+        childJSON.setKeyValuePair("clientType", value: "Swift-Test-Client");
+        childJSON.setKeyValuePair("clientVersion", value: "0.21");
+        childJSON.setKeyValuePair("module", value: "OS X Prototype");
+        childJSON.setKeyValuePair("userID", value: "PDPS-WS2015")
+        
+        json.setKeyValuePair("origin", value: childJSON.jsonObject)
+        json.setKeyValuePair("numResults", value: 1)
+        
+        let childJSON2 = JSON()
+        childJSON2.setKeyValuePair("text", value: "\(recommendation.stringValue)")
+        json.setKeyValuePair("contextKeywords", value: [childJSON2.jsonObject])
+        print(json.convertToString())
+        
+        self.connectionManager.makeHTTP_Request(json, url: "https://eexcess-dev.joanneum.at/eexcess-privacy-proxy-1.0-SNAPSHOT/api/v1/recommend", httpMethod: ConnectionManager.POST, postCompleted: { (succeeded: Bool, msg: NSData) -> () in
+                        if(succeeded) {
+                            dispatch_async(dispatch_get_main_queue(), {
+                                self.response.string = String(data: msg, encoding: NSUTF8StringEncoding)!
+                                self.msg = msg
+                            })
+                        }
+                        else {
+                            self.response.string = "Error"
+                        }
+                    })
     }
      override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        connection = Connection()
+        
+//        connection = Connection()
+        self.connectionManager = ConnectionManager()
         recommendation.delegate = self
     }
 
