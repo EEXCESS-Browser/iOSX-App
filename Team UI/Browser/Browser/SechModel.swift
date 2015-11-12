a40b544d16f06002655eb337c82bd018471711fd
feres21 <brian.mairhoerman@hof-university.de>
2015-11-12 18:00:03 +0100

    Sechmodel erstellt

--- Team UI/Browser/Browser/SechModel.swift
+++ Team UI/Browser/Browser/SechModel.swift
@@ -2,16 +2,29 @@
 //  SechModel.swift
 //  Browser
 //
-//  Created by Andreas Ziemer on 21.10.15.
+//  Created by Brian Mairhörmann on 21.10.15.
 //  Copyright © 2015 SECH-Tag-EEXCESS-Browser. All rights reserved.
 //
 
 import Foundation
 
-class SechModel{
-    var tags: [String]
-    
-    init(mTags: [String]){
-        self.tags = mTags
-    }
+class Sech{
+    let id = String()
+    let response = String()
+    let detail = String()
+    let tags = [String : Tag]()
+    let filters = [String : Filter]()
 }
+
+class Tag {
+    let topic = String()
+    let type = String()
+    let isMainTopic = false
+}
+
+class Filter{
+    let mediaType = String()
+    let provider = String()
+    let licence = String()
+}
+
