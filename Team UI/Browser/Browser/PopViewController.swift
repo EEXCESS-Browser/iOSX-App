55260aacfcf14ee3ea2351fb2222ee966f689291
philippwinterholler <philipp.winterholler@hof-university.de>
2016-01-08 11:22:53 +0100

    popOver TableView eingefügt

--- Team UI/Browser/Browser/PopViewController.swift
+++ Team UI/Browser/Browser/PopViewController.swift
@@ -20,6 +20,37 @@ class PopViewController : UIViewController{
     @IBOutlet weak var sechImage: UIImageView!
     @IBOutlet weak var sechWebView: UIWebView!
     
+    @IBAction func openTable(sender: AnyObject) {
+//        var popoverContent = (self.storyboard?.instantiateViewControllerWithIdentifier("SearchTableViewController"))! as UIViewController
+//        var nav = UINavigationController(rootViewController: popoverContent)
+//        nav.modalPresentationStyle = UIModalPresentationStyle.Popover
+//        var popover = nav.popoverPresentationController
+//        popoverContent.preferredContentSize = CGSizeMake(500,600)
+//       // popover!.delegate = self.view
+//        popover!.sourceView = self.view
+//        popover!.sourceRect = CGRectMake(100,100,0,0)
+//        
+//        self.presentViewController(nav, animated: true, completion: nil)
+        let popoverContent = (self.storyboard?.instantiateViewControllerWithIdentifier("SearchTableViewController"))! as UIViewController
+        
+        popoverContent.modalPresentationStyle = .Popover
+        var popover = popoverContent.popoverPresentationController
+        
+        if let popover = popoverContent.popoverPresentationController {
+            
+            let viewForSource = sender as! UIView
+            popover.sourceView = viewForSource
+            
+            // the position of the popover where it's showed
+            popover.sourceRect = viewForSource.bounds
+            
+            // the size you want to display
+            popoverContent.preferredContentSize = CGSizeMake(200,500)
+//            popover.delegate = self.view
+        }
+        
+        self.presentViewController(popoverContent, animated: true, completion: nil)
+    }
     
     var headLine : String!
     var jsonText : String!
